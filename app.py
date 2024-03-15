137de1bbfe8e0ba25a08aebfab32b3082634bf60
Justin17z <142373769+justin17z@users.noreply.github.com>
2024-03-15 20:40:57 +0800

    app.py

--- app.py
+++ app.py
@@ -1,22 +1,50 @@
 from flask import Flask, request, render_template
+import datetime
+import sqlite3
+from flask import Markup
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+name_flag = 0
+name=""
+
+@app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
 
-@app.route("/main", methods=["GET", "POST"])
+@app.route("/main",methods=["GET","POST"])
 def main():
-    name = request.form.get("name")
-    return(render_template("main.html", name=name))
+    global name_flag,name
+    if name_flag==0:
+        name = request.form.get("name")
+        name_flag=1
+        conn = sqlite3.connect("log.db")
+        c = conn.cursor()
+        timestamp = datetime.datetime.now()
+        c.execute("insert into employee (name,timestamp) values(?,?)",(name,timestamp))
+        conn.commit()
+        c.close()
+        conn.close()
+    return(render_template("main.html",name=name))
 
-@app.route("/ethical_test", methods=["GET", "POST"])
-def ethical_test():
-
+@app.route("/ethical_test",methods=["GET","POST"])
+def ethical_test():   
     return(render_template("ethical_test.html"))
 
-@app.route("/answer", methods=["GET", "POST"])
+@app.route("/query",methods=["GET","POST"])
+def query():
+    conn = sqlite3.connect("log.db")
+    c = conn.execute("select * from employee")
+    r=""
+    for row in c:
+        r=r+str(row)+"<br>"
+    print(r)
+    r = Markup(r)
+    c.close()
+    conn.close()
+    return(render_template("query.html",r=r))
+
+@app.route("/answer",methods=["GET","POST"])
 def answer():
     ans = request.form["options"]
     print(ans)
@@ -25,11 +53,9 @@ def answer():
     else:
         return(render_template("correct.html"))
 
-
-@app.route("/end", methods=["GET", "POST"])
-def end():
-
+@app.route("/end",methods=["GET","POST"])
+def end():  
     return(render_template("end.html"))
 
 if __name__ == "__main__":
-    app.run(port=5050)
+    app.run()
